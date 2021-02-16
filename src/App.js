e96267cb0f1ca0a53fd86e10a5ceddacc3a6f463
JUNGMIN.Y <y.jung367@gmail.com>
2021-02-16 23:51:52 +0900

    update
    Fetched data from API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async() => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json")
+  }
+  componentDidMount() {
+    this.getMovies();
+  }
+  axios
+  render(){
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+  }
 }
 
 export default App;
